59bd794d9eb7ead3608f632c624221f0fc336dbf
gythaogg <gythaoggscat@gmail.com>
2020-11-28 08:30:40 +0100

    Added routing to ern slides

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { QuizComponent } from './quiz/quiz.component';
 import { HomeComponent } from './home/home.component';
+import { PresentationComponent } from './presentation/presentation.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'quiz', component: QuizComponent },
+  { path: 'ernslides', component: PresentationComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
